26a046b4ebded2652fd207ee70804423a25ba1cf
Zoltán Hrabovszki <hrabovszki@googlemail.com>
2016-06-23 07:10:42 +0200

    Merged

---
+++ core/src/main/java/okw/OKW.java
@@ -0,0 +1,137 @@
+package okw;
+
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+ 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+ 
+/// \~german
+/// \brief Annotation @Interface für die Zuordnung des Fachlichen Name-s eines GUI-Objektes im Frame.
+/// \see; http://tutorials.jenkov.com/java-reflection/annotations.html
+/// == github Issue
+/// issue https://github.com/Hrabovszki1023/OKW/issues/37
+///
+/// \~english
+/// \brief
+///
+/// \~
+/// \author Zoltan Hrabovszki
+/// \date 2015-11-28
+ 
+@Target( { ElementType.TYPE,     // Usable in Class-es
+           ElementType.FIELD } ) // Usable in Field-s
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+public @interface OKW {
+ 
+        /// \~german
+    /// \brief_F_untionaler _N_ame des GUI-Objektes
+    ///
+    /// \~english
+    /// \brief Holds the Functional Name of the GUI-Object.
+    ///
+    /// \~
+    /// \author Zoltan Hrabovszki
+    /// \date 2015-11-28
+        String FN() default "";
+ 
+ 
+        /**
+        * TimeOut in Secunden [s] für das Schlüsselwort VerifyHasFocus.
+        * Default ist 30s.
+        * in [s]
+        */
+        int VerifyHasFocus_TO() default 30;
+       
+        /**
+        * Polling Time in [ms].
+        *
+        * Die Periodendauer des Pollens wird in Millisekunden angegeben.
+        */
+        int VerifyHasFocus_PT() default 1000;
+ 
+       
+        /**
+        * TimeOut in [s]
+        */
+        int VerifyIsActive_TO() default 30;
+       
+        /**
+        * Polling Time in [ms].
+        *
+        * Die Periodendauer des Pollens wird in Millisekunden angegeben.
+        */
+        int VerifyIsActive_PT() default 1000;
+ 
+ 
+        /**
+        * TimeOut
+        * in [s]
+        */
+        int VerifyLabel_TO() default 30;
+       
+        /**
+        * Polling Time in [ms].
+        *
+        * Die Periodendauer des Pollens wird in Millisekunden angegeben.
+        */
+        int VerifyLabel_PT() default 1000;
+ 
+       
+        /**
+        * TimeOut
+        * in [s]
+        */
+        int VerifySelectedValue_TO() default 30;
+       
+        /**
+        * Polling Time in [ms].
+        *
+        * Die Periodendauer des Pollens wird in Millisekunden angegeben.
+        */
+        int VerifySelectedValue_PT() default 1000;
+ 
+       
+        /**
+        * TimeOut
+        * in [s]
+        */
+        int VerifyTooltip_TO() default 30;
+       
+        /**
+        * Polling Time in [ms].
+        *
+        * Die Periodendauer des Pollens wird in Millisekunden angegeben.
+        */
+        int VerifyTooltip_PT() default 1000;
+ 
+       
+        /**
+        * TimeOut in [s]
+        */
+        int VerifyValue_TO() default 30;
+       
+        /**
+        * Polling Time in [ms].
+        *
+        * Die Periodendauer des Pollens wird in Millisekunden angegeben.
+        */
+        int VerifyValue_PT() default 1000;
+ 
+       
+        /**
+        * TimeOut
+        * in [s]
+        */
+        int VerifyExists_TO() default 30;
+       
+        /**
+        * Polling Time in [ms].
+        *
+        * Die Periodendauer des Pollens wird in Millisekunden angegeben.
+        */
+        int VerifyExist_PT() default 1000;
+       
+}
