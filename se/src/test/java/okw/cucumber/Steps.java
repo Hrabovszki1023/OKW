d01807b53d85c8ce8c9844d4fdfca0d10ee1c9fd
Hrabovszki1023 <zh@openkeyword.de>
2016-08-08 23:47:18 +0200

    Cucumber Example updated

--- se/src/test/java/okw/cucumber/Steps.java
+++ se/src/test/java/okw/cucumber/Steps.java
@@ -7,48 +7,72 @@ import cucumber.api.java.en.When;
 
 public class Steps
 {
-  @Given("^Calculator is started$")
-  public void calculator_is_started() throws Throwable {
-      EN.StartApp( "Firefox" );
-      EN.TypeKey( "URL", "http://www.schulferien.org/taschenrechner/taschenrechner.html");
-  }
   
   @Then("^I Close Calculator$")
   public void close_Calculator() throws Throwable {
       EN.StopApp( "Firefox" );
   }
 
-  @Given("^The Calculator exists$")
-  public void the_Calculator_exists() throws Throwable {
-    EN.SelectWindow("Calculator");    
-    EN.VerifyExists( "Display", "YES" );
-  }
-
-  @When("^I enter \"([^\"]*)\" into the calculator$")
-  public void i_enter_into_the_calculator(String arg1) throws Throwable {
-    
-    EN.SelectWindow( "Calculator" );
-
-    for( int i=0; i< arg1.length(); i++)
+  public void typecalculator(String arg1) throws Throwable{
+        for( int i=0; i< arg1.length(); i++)
     {  
       EN.ClickOn( Character.toString(arg1.charAt(i)) );
     }
   }
-
-  @When("^I press \"([^\"]*)\"$")
-  public void i_press(String arg1) throws Throwable {
-    
+  
+  @Given("^I use the calculater$")
+  public void i_use_the_calculater() throws Throwable{
+    EN.StartApp( "Firefox" );
+    EN.TypeKey( "URL", "http://www.schulferien.org/taschenrechner/taschenrechner.html");
     EN.SelectWindow( "Calculator" );
-    EN.ClickOn( arg1 );
 
   }
 
-  @Then("^the Calculator displays \"([^\"]*)\"$")
-  public void the_Calculatron_displayy(String arg1) throws Throwable {
-    
+  @When("^I enter \"([^\"]*)\" into the calculator$")
+  public void i_enter_into_the_calculator(String arg1) throws Throwable {
+    this.typecalculator( arg1 );
+   }
+  
+  @When("^I add \"([^\"]*)\"$")
+  public void i_add(String arg1) throws Throwable{
+    EN.SelectWindow( "Calculator" );
+
+    EN.ClickOn( "+" );
+    this.typecalculator( arg1 );
+    EN.ClickOn( "=" );
+  }
+
+  @When("^I subtract \"([^\"]*)\"$")
+  public void i_subtract(String arg1) throws Throwable{
+    EN.SelectWindow( "Calculator" );
+
+    EN.ClickOn( "-" );
+    this.typecalculator( arg1 );
+    EN.ClickOn( "=" );
+  }
+
+  @When("^I divide by \"([^\"]*)\"$")
+  public void i_divide_by(String arg1) throws Throwable {
+    EN.SelectWindow( "Calculator" );
+
+    EN.ClickOn( "/" );
+    this.typecalculator( arg1 );
+    EN.ClickOn( "=" );
+  }
+
+  @When("^I multiply by \"([^\"]*)\"$")
+  public void i_multiply_by(String arg1) throws Throwable {
+      EN.SelectWindow( "Calculator" );
+
+      EN.ClickOn( "*" );
+      this.typecalculator( arg1 );
+      EN.ClickOn( "=" );
+  }
+
+  @Then("^the calculated result is \"([^\"]*)\"$")
+  public void the_calculated_result_is(String arg1) throws Throwable {
     EN.SelectWindow( "Calculator" );
     EN.VerifyValue( "Display", arg1 );
-
   }
 }
 
--- se/src/test/resources/myFeature.feature
+++ se/src/test/resources/myFeature.feature
@@ -1,11 +1,10 @@
 Feature: This is my dummyfeature file
 
-  Scenario: Add two numbers
-    Given Calculator is started
-    And The Calculator exists
-    When I enter "123" into the calculator
-    And I press "+"
-    And I enter "12" into the calculator
-    And I press "="
-    Then the Calculator displays "135"
-    And I Close Calculator
+  Scenario: Calculating with the four basic arithmetic operations
+    Given I use the calculater
+    When I enter "1" into the calculator
+    And I add "200"
+    And I subtract "200"
+    And I divide by "555"
+    And I multiply by "555"
+    Then the calculated result is "1"
