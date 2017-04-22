f84eecac127cad8f4c49ce8b083abd839eb3fecd
Hrabovszki1023 <zh@openkeyword.de>
2017-04-22 07:43:28 +0200

    next try

--- InstallDoxygen.sh
+++ InstallDoxygen.sh
@@ -10,4 +10,4 @@ DOXYGEN_URL="http://ftp.stack.nl/pub/users/dimitri/${DOXYGEN_TAR}"
 mkdir -p $HOME/local/bin
 wget -O - "${DOXYGEN_URL}" | tar xz -C ${TMPDIR-/tmp} ${DOXYGEN_VER}/bin/doxygen
 mv ${TMPDIR-/tmp}/${DOXYGEN_VER}/bin/doxygen $HOME/local/bin/doxygen
-export PATH=$PATH:$HOME/local/bin/doxygen
+export PATH=$PATH:$HOME/local/bin
