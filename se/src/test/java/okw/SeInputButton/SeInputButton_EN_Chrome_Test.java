cf72c1ff76ba990fb242ad8db6485dc06df6546a
hrabovszki2310 <zh@openkeyword.de>
2019-04-14 09:25:35 +0200

    add chrome testcaes to testexecution

--- se/src/test/java/okw/SeInputButton/SeInputButton_EN_Chrome_Test.java
+++ se/src/test/java/okw/SeInputButton/SeInputButton_EN_Chrome_Test.java
@@ -29,7 +29,7 @@ public class SeInputButton_EN_Chrome_Test extends SeInputButton_EN_Test
         myLog2HTML = new Log2HTML("target/SeInputButton_Chrome_Test.html");
         Logger_Sngltn.getInstance().addLogger(myLog2HTML);
 
-        Logger_Sngltn.getInstance().setDebugMode(true);
+        Logger_Sngltn.getInstance().setDebugMode(false);
 	}
 
     /** @After
