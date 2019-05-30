5433f1fae97cf9a821af2c01524edc1759f64fdf
hrabovszki2310 <zh@openkeyword.de>
2019-05-30 13:19:09 +0200

    Add File testcases to AllCoreTestssuits

--- core/src/test/java/okw/core/AllCoreTests.java
+++ core/src/test/java/okw/core/AllCoreTests.java
@@ -46,6 +46,7 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith( Suite.class )
 @SuiteClasses(
 { CurrentObject_Test.class,
+  EN_FileKeywords_Test.class,
   EN_Keywords_ANTLR_Test.class,
   EN_Keywords_DELETE_Test.class,
   EN_Keywords_Empty_Test.class,
