5a95fc350b7bf3271d329ef78e5029b185d6bb92
hrabovszki2310 <zh@openkeyword.de>
2019-11-29 05:40:58 +0100

    Update

---
+++ core/src/test/java/okw/OKW_Memorize_Sngltn_Test.java
@@ -0,0 +1,110 @@
+package okw;
+
+import static org.junit.Assert.*;
+
+import javax.xml.xpath.XPathExpressionException;
+
+import okw.exceptions.*;
+import okw.log.Logger_Sngltn;
+
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+@FixMethodOrder( MethodSorters.NAME_ASCENDING )
+public class OKW_Memorize_Sngltn_Test
+{
+
+        public static OKW_Memorize_Sngltn myOKW_Memorize;
+
+        // / \copydoc CurrentObject::Log()
+        private static Logger_Sngltn Log = Logger_Sngltn.getInstance();
+
+        @BeforeClass
+        public static void mySetUp()
+        {
+            myOKW_Memorize = OKW_Memorize_Sngltn.getInstance();
+
+            // Reset des Loggers: Alle geladenen Instanzen löschen
+            Logger_Sngltn.init();
+
+            // Log.AddLogger(new Log2Console());
+            Log.setDebugMode( false );
+        }
+
+        
+        /**
+         *  Prüft die Methoden get und set, ohne
+         *  wenn ein nicht vorhandener Internalname verwendet wird.<br/>
+         * @throws XPathExpressionException 
+         */
+        @Test
+        public void tc_get_set() throws XPathExpressionException
+        {
+            myOKW_Memorize.reset();
+            myOKW_Memorize = OKW_Memorize_Sngltn.getInstance();
+            
+            myOKW_Memorize.set( "Key 1", "Value 1" );
+            String myValue = myOKW_Memorize.get( "Key 1" );
+            assertEquals( "Value 1", myValue );
+        }
+
+        
+        /**
+         *  Prüft die Methoden get und set, ohne
+         *  wenn ein nicht vorhandener Internalname verwendet wird.<br/>
+         * @throws XPathExpressionException 
+         */
+        @Test(expected=OKWMemorizeKeyNotExistsException.class)
+        public void tc_get_OKWMemorizeKeyNotExistsException() throws XPathExpressionException
+        {
+            myOKW_Memorize.reset();
+            myOKW_Memorize = OKW_Memorize_Sngltn.getInstance();
+            
+            myOKW_Memorize.set( "Key 1", "Value 1" );
+            String myValue = myOKW_Memorize.get( "Key 1" );
+            assertEquals( "Value 1", myValue );
+            
+            myValue = myOKW_Memorize.get( "Key 2" );
+            
+        }
+        
+        /**
+         *  Prüft die Methoden OKW_Memorize_Sngltn.exist(String)
+         */
+        @Test
+        public void tc_exists() throws XPathExpressionException
+        {
+            myOKW_Memorize.reset();
+            myOKW_Memorize = OKW_Memorize_Sngltn.getInstance();
+            
+            myOKW_Memorize.set( "Key 1", "Value 1" );
+
+            assertEquals( true, myOKW_Memorize.exists( "Key 1" ) );
+            
+            assertEquals( false, myOKW_Memorize.exists( "Key DoesnotExists" ) );
+            
+            
+        }
+        
+        /**
+         *  Prüft die OKW_Memorize_Sngltn.Set(String,String),
+         *  wenn ein Schlüssel bereit existiert und mit einem
+         *  neuen Wert belegt wird
+         */
+        @Test
+        public void tc_set_aKey_twice() throws XPathExpressionException
+        {
+            myOKW_Memorize.reset();
+            myOKW_Memorize = OKW_Memorize_Sngltn.getInstance();
+            
+            myOKW_Memorize.set( "Key 1", "Value 1" );
+            assertEquals( "Value 1", myOKW_Memorize.get( "Key 1" ) );
+            
+            myOKW_Memorize.set( "Key 1", "Value 2" );
+            assertEquals( "Value 2", myOKW_Memorize.get( "Key 1" ) );
+            
+            
+        }
+}
