42e4181aeebb59eb6ff1f0338d14eadde8753e33
Bernát Kalló <kallo.bernat@gmail.com>
2020-09-23 17:46:41 +0200

    SeAriaCheckbox

---
+++ se/src/main/java/okw/gui/adapter/selenium/SeAriaCheckbox.java
@@ -0,0 +1,23 @@
+package okw.gui.adapter.selenium;
+
+import okw.gui.OKWLocatorBase;
+
+public class SeAriaCheckbox extends SeCheckbox {
+	public SeAriaCheckbox(String Locator, OKWLocatorBase[] Locators) {
+		super(Locator, Locators);
+	}
+
+	@Override
+	public Boolean getIsSelected() {
+		Boolean lvbReturn = false;
+
+		try {
+			LogFunctionStartDebug("getIsSelected");
+
+			lvbReturn = this.Me().getAttribute("aria-checked").equalsIgnoreCase("true");
+		} finally {
+			LogFunctionEndDebug(lvbReturn);
+		}
+		return lvbReturn;
+	}
+}
